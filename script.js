61ce045ff0f3a069248a39b21be7dfcc949c22e4
webQbe <webcubesolutionslk@gmail.com>
2024-11-17 21:04:19 +0530

    Defined globals

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ const slider = document.querySelector('.slider-container'),
 
 /* Convert node list into an array with Array.from() */
 slides = Array.from(document.querySelectorAll('.slide')) 
+
+// Globals
+let isDragging = false, // Mouse-down & moving
+    startPos = 0, // Position clicked
+    currentTranslate = 0, // TranslateX(value)
+    prevTranslate = 0, 
+    animationID = 0, 
+    currentIndex = 0 // Current slide
+
+
