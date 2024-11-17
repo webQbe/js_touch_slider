12f1427111d7210bca4847b540f422f23ea69635
webQbe <webcubesolutionslk@gmail.com>
2024-11-17 20:34:34 +0530

    Defined DOM Elements

---
+++ script.js
@@ -0,0 +1,5 @@
+// Define DOM Elements
+const slider = document.querySelector('.slider-container'),
+
+/* Convert node list into an array with Array.from() */
+slides = Array.from(document.querySelectorAll('.slide')) 
